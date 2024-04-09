4dd2fe8bc3fd2cfaba9b104789bcb7ad868c4c6a
tigad049 <115029111+tigad049@users.noreply.github.com>
2024-04-08 20:41:45 -0700

    Add more functions to controller

--- shibe-generator/controller.py
+++ shibe-generator/controller.py
@@ -5,16 +5,71 @@ Python code to make API connections.
 """
 
 import requests as re
+import json
+import shutil
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import (
     QLabel,
     QVBoxLayout,
 )
 
-url = "http://shibe.online/api/shibes"
-response = re.get(url)
+url = "http://shibe.online/api/shibes?urls=false"
 
-if response.ok:
-    print(response.text)
-else:
-    print(f"There was an error: {response.status_code}")
+def get_image() -> str:
+    """Gets an image ID and URL from shibe.online.
+
+    Returns:
+        image_id: The image ID used in shibe.online's CDN.
+
+        image_url = The URL on shibe.online's CDN.
+    """
+    response = re.get(url)
+    
+    if response.ok:
+        data = json.loads(response.text)
+        image_id = data[0]
+        image_url = f"https://cdn.shibe.online/shibes/{image_id}.jpg"
+        image_response = re.get(image_url, stream = True)
+
+        if image_response.ok:
+            return image_id, image_url
+
+        else:
+            print(f"There was an error accessing the image: {response.status_code}")
+
+    else:
+        print(f"There was an error accessing the API: {response.status_code}")
+
+def download_image(image_id, image_url):
+    """Downloads an image from shibe.online.
+
+    Arguments:
+        image_id: The image ID used in shibe.online's CDN.
+
+        image_url = The URL on shibe.online's CDN.
+    """
+    image_response = re.get(image_url, stream = True)
+    if image_response.ok:
+        with open(f"cache/{image_id}.jpg",'wb') as f:
+            shutil.copyfileobj(image_response.raw, f)
+        print(f"Image donwloaded: {image_id}.jpg")
+    else:
+        print(f"There was an error accessing the image: {response.status_code}")
+
+def get_vbox(image_id) -> QVBoxLayout:
+    """Returns a vbox layout with an image from shibe.online.
+
+    Arguments:
+        image_id: The image ID used in shibe.online's CDN.
+
+    Returns:
+        image_layout: A vbox layout with an image from shibe.online.
+    """
+    image_layout = QVBoxLayout()
+    folder = "cache/"
+    filepath = f"{folder}{image_id}.jpg"
+    print(filepath)
+    image = QPixmap(filepath)
+
+    image_layout.addWidget(image)
+    return image_layout
