c07f950e4526845ace1cd0d1ab5fb5640caf5cdb
tigad049 <115029111+tigad049@users.noreply.github.com>
2024-04-08 22:06:20 -0700

    Add image switching

--- shibe-generator/app.py
+++ shibe-generator/app.py
@@ -31,21 +31,24 @@ class MainWindow(QMainWindow):
 
         # Title
         title_label = QLabel("Shibe Generator")
-        title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        title_label.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignCenter)
         title_label.setStyleSheet("font: bold 30pt Verdana")
 
         # Shibe Image
-        image_label = QLabel(self)
+        self.image_label = QLabel(self)
         pixmap = QPixmap(f"cache/{image_id}.jpg")
-        image_label.setPixmap(pixmap)
+        self.image_label.setPixmap(pixmap)
+        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         # Get Shibe button
         self.get_shibe_button = QPushButton("Get a Shibe")
-        self.get_shibe_button.setStyleSheet("font: 10pt Verdana")
+        self.get_shibe_button.setStyleSheet("font: 15pt Verdana")
+
+        self.get_shibe_button.clicked.connect(self.new_image)
 
         # Add widgets to the layout
         layout.addWidget(title_label)
-        layout.addWidget(image_label)
+        layout.addWidget(self.image_label)
         layout.addWidget(self.get_shibe_button)
 
         widget = QWidget()
@@ -53,6 +56,12 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
 
         self.setFixedSize(QSize(720, 720))
+    
+    def new_image(self):
+        image_id, image_url = controller.get_image()
+        controller.download_image(image_id, image_url)
+        pixmap = QPixmap(f"cache/{image_id}.jpg")
+        self.image_label.setPixmap(pixmap)
 
 
 class Color(QWidget):
